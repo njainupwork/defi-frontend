ad4a618d9d5e1f3efad3d016207fa464ea870d6e
lion cho <chowin21@gmail.com>
2021-10-21 17:44:02 +0900

    ci(test): Info file issue

--- cypress/integration/info/info.test.ts
+++ cypress/integration/info/info.test.ts
@@ -11,6 +11,8 @@ describe('Info Page', () => {
 
   it('loads single pool page', () => {
     cy.visit('/info/pool/0x58f876857a02d6762e0101bb5c46a8c1ed44dc16')
+    // Sometime no issue, sometime works, due to network issue.
+    cy.wait(20000)
     cy.get('#info-pool-pair-title').should('be.visible')
   })
 
