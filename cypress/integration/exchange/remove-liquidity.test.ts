a38b817b6d9be9e32b4e4e8e6f861a55fa533a34
lion cho <chowin21@gmail.com>
2021-10-31 15:57:52 +0900

    test: Clear issue

--- cypress/integration/exchange/remove-liquidity.test.ts
+++ cypress/integration/exchange/remove-liquidity.test.ts
@@ -10,18 +10,18 @@ describe('Remove Liquidity', () => {
   it('bnb-cake remove', () => {
     cy.visit('/remove/BNB/0x2904940De789B2034D5bA883bD049BA71d54Ef51')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'BNB')
-    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'CAKE')
+    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'DOODA')
   })
 
   it('cake-bnb remove', () => {
     cy.visit('/remove/0x2904940De789B2034D5bA883bD049BA71d54Ef51/BNB')
-    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
+    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'DOODA')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'BNB')
   })
 
   it('loads the two correct tokens', () => {
     cy.visit('/remove/0x2904940De789B2034D5bA883bD049BA71d54Ef51/0xC0063D362A8EA40Cb32A774c75683429c2B4860c')
-    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
+    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'DOODA')
     cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'BUSD')
   })
 
@@ -33,13 +33,13 @@ describe('Remove Liquidity', () => {
 
   it('does not crash if token is duplicated', () => {
     cy.visit('/remove/0x2904940De789B2034D5bA883bD049BA71d54Ef51/0x2904940De789B2034D5bA883bD049BA71d54Ef51')
-    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'CAKE')
-    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'CAKE')
+    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'DOODA')
+    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'DOODA')
   })
 
   it('token not in storage is loaded', () => {
-    cy.visit('/remove/0xD74b782E05AA25c50e7330Af541d46E18f36661C/0x2904940De789B2034D5bA883bD049BA71d54Ef51')
-    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'QUACK')
-    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'CAKE')
+    cy.visit('/remove/0x637A86aFf37dF6cD3c6c16439a4bdeE32272Fdc7/0x2904940De789B2034D5bA883bD049BA71d54Ef51')
+    cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'TROAD')
+    cy.get('#remove-liquidity-tokenb-symbol').should('contain.text', 'DOODA')
   })
 })
