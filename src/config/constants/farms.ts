5a1f0055672dc6bff3b352b92f863351fdd6077a
lion cho <chowin21@gmail.com>
2021-11-01 21:23:58 +0900

    fix(farm): New LP

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -40,6 +40,16 @@ const farms: SerializedFarmConfig[] = [
   /**
    * V3 by order of release (some may be out of PID order due to multiplier boost)
    */
+  {
+    pid: 4,
+    lpSymbol: 'Troad-BNB LP',
+    lpAddresses: {
+      97: '0xF4d81360FC84226816c8711E0Cb432484D301BC7',
+      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+    },
+    token: serializedTokens.troad,
+    quoteToken: serializedTokens.wbnb,
+  },
 ]
 
 export default farms
