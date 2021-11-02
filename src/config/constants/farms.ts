ab8198ad97d68cd7c5a5fa6eda075229ff214cec
Nikhil Jain <nikhiljainupwork@gmail.com>
2021-11-02 16:56:02 +0530

    fix: Masterchef adding new farms issue

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -31,20 +31,20 @@ const farms: SerializedFarmConfig[] = [
     pid: 2,
     lpSymbol: 'Dooda-BNB LP',
     lpAddresses: {
-      97: '0xeEE9b2aae625Cb5060F9fab22339b9a1A4739040',
+      97: '0x5795427f57Ad3cd0Effe4fc8d9713fF06b07089B',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
     },
     token: serializedTokens.dooda,
     quoteToken: serializedTokens.wbnb,
   },
-  /**
-   * V3 by order of release (some may be out of PID order due to multiplier boost)
-   */
+  // /**
+  //  * V3 by order of release (some may be out of PID order due to multiplier boost)
+  //  */
   {
-    pid: 4,
+    pid: 3,
     lpSymbol: 'Troad-BNB LP',
     lpAddresses: {
-      97: '0xF4d81360FC84226816c8711E0Cb432484D301BC7',
+      97: '0x93c0cA08DFCc5EeBf52d2741aE4b44626303Daf5',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
     },
     token: serializedTokens.troad,
